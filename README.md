a4662bd6c3a0868b9596a4ca2651730ad1bbbe43
Akshay Asok <akshayasokson@gmail.com>
2025-06-05 16:00:44 +0530

    requirement implemented

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Full name display buildout
 
 ----------------------
 Requirements
+1. After entering first and last name, and clicking on the submit button, you must get the full name displayed 
+2. You must make the input fields such that the user is unable to submit without filling in both the fields.
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,57 @@
+import { useState } from "react";
+
+
+
+// form component
+function NameForm({name, setName}) {
+
+
+
+  return (
+    <form name="name" 
+    onSubmit={(e) => {
+      e.preventDefault();
+      // console.log("submit event obj >> ",e);
+      setName({
+        firstName : e.target.elements.first.value,
+        lastName : e.target.elements.last.value
+      })
+    }}
+    >
+      <div>
+        <label for="first">First Name:</label>
+        <input name="first" id="first" required />
+      </div>
+      
+      <div>
+        <label for="last" id="first">Last Name:</label>
+        <input name="last" required />
+      </div>
+
+      <button type="submit" >Submit</button>
+    </form>
+  )
+}
+
 
 
 function App() {
+
+  const [name, setName] = useState({
+    firstName : "",
+    lastName : ""
+  })
+
+
   return (
     <div className="App">
-      full name display
+      <h1>Full Name Display</h1>
+      
+      <NameForm name={name} setName={setName} />
+      
+      {
+        (name.firstName.length>0 && name.lastName.length>0) && (<h4>{`Full Name: ${name.firstName} ${name.lastName}`}</h4>)
+      }
     </div>
   );
 }
